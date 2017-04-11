1a4ff0d930f57c04b03a9cf34e9071e51592110e
Dmitry Myadzelets <dmitrymyadzelets@gmail.comgit config --global user.email dmitrymyadzelets@gmail.com>
2017-04-11 23:33:20 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,117 @@
+/*jslint browser: false*/
+'use strict';
+
+var url = require('url');
+
+
+// ============================================================================
+// Helpers
+
+
+// Returns a copy of the object
+function copy(object, dest) {
+    return Object.keys(object).reduce(function (o, key) {
+        o[key] = object[key];
+        return o;
+    }, dest || {});
+}
+
+
+// HTTP response callback
+function response(cb, res) {
+    var body;
+
+    function data(d) {
+        if (undefined === body) {
+            body = '';
+        }
+        body += d;
+    }
+
+    function end() {
+        // Parse body as JSON if necessary
+        const type = '' + res.headers['content-type'];
+        const isJSON = type.includes('application/json');
+        if (isJSON) {
+            try {
+                body = JSON.parse(body);
+            } catch (err) {
+                cb(err);
+            }
+        }
+        cb(null, res, body);
+        body = null;
+    }
+
+    res.setEncoding('utf8').on('data', data).on('end', end);
+}
+
+
+// Template for REST methods
+function method(opt, data, cb, path) {
+    if ('function' === typeof data) {
+        cb = data;
+        data = undefined;
+    }
+    // Add path if any
+    if (path) {
+        opt = copy(opt);
+        opt.path += path;
+    }
+    const req = this.http.request(opt, response.bind(this, cb));
+    req.on('error', cb);
+    if (undefined !== data) {
+        req.write(JSON.stringify(data));
+    }
+    req.end();
+}
+
+
+// ============================================================================
+//
+function Remote(http, uri) {
+    this.http = http;
+
+    const parsed = url.parse(uri);
+    this.options = {
+        get: {
+            host: parsed.host,
+            path: parsed.path,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    };
+    this.options.post = copy(this.options.get, {method: 'POST'});
+    this.options.put = copy(this.options.get, {method: 'PUT'});
+    this.options.patch = copy(this.options.get, {method: 'PATCH'});
+    this.options.del = copy(this.options.get, {method: 'DELETE'});
+}
+
+
+Remote.prototype.get = function (data, cb, path) {
+    return method.call(this, this.options.get, data, cb, path);
+};
+
+Remote.prototype.post = function (data, cb, path) {
+    return method.call(this, this.options.post, data, cb, path);
+};
+
+Remote.prototype.put = function (data, cb, path) {
+    return method.call(this, this.options.put, data, cb, path);
+};
+
+Remote.prototype.patch = function (data, cb, path) {
+    return method.call(this, this.options.patch, data, cb, path);
+};
+
+Remote.prototype.del = function (data, cb, path) {
+    return method.call(this, this.options.del, data, cb, path);
+};
+
+
+// ============================================================================
+// Module exports
+exports.remote = function (http, uri) {
+    return new Remote(http, uri);
+};
---
+++ tests/test.js
@@ -0,0 +1,93 @@
+/*jslint browser: false*/
+'use strict';
+
+const http = require('http');
+const https = require('https');
+const assert = (function () {
+    var cnt = 0;
+    return function () {
+        require('assert').apply(this, arguments);
+        cnt += 1;
+        console.log('passed ' + cnt);
+    };
+}());
+
+// Ignore HTTPS warnings
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+
+console.log('Expected ' + 28 + ' tests passed');
+
+
+const remote = require('../').remote;
+const json = remote(https, 'https://jsonplaceholder.typicode.com/posts');
+
+
+// http options
+assert(json.options.get.host === 'jsonplaceholder.typicode.com');
+assert(json.options.get.path === '/posts');
+
+
+// get
+json.get(function (err, res, body) {
+    assert(!err);
+    assert(200 === res.statusCode);
+    assert(body instanceof Array);
+    assert(100 === body.length);
+});
+
+
+// post
+json.post({ok: true}, function (err, res, body) {
+    assert(!err);
+    assert(201 === res.statusCode);
+    assert(true === body.ok);
+});
+
+
+// put
+json.put({ok: true}, function (err, res, body) {
+    assert(!err);
+    assert(200 === res.statusCode);
+    assert(true === body.ok);
+    assert(1 === body.id);
+}, '/1');
+
+
+// patch
+json.patch({ok: true}, function (err, res, body) {
+    assert(!err);
+    assert(200 === res.statusCode);
+    assert(true === body.ok);
+    assert('string' === typeof body.title);
+    assert(2 === body.id);
+}, '/2');
+
+
+json.del({}, function (err, res, body) {
+    assert(!err);
+    assert(200 === res.statusCode);
+    assert(!!body);
+}, '/3');
+
+
+// Some additional tests
+(function () {
+
+    remote(https, 'https://wrong.address').get(function (err) {
+        assert(err instanceof Error);
+    });
+
+    remote(http, 'http://echo.jsontest.com/key/value/one/two').get(function (err, res, body) {
+        assert(!err);
+        assert(200 === res.statusCode);
+        assert('object' === typeof body);
+    });
+
+    remote(http, 'http://validate.jsontest.com/?json=%7B%22key%22:%22value%22').get(function (err, res, body) {
+        assert(!err);
+        assert(200 === res.statusCode);
+        assert('This error came from the org.json reference parser.' === body.error_info);
+    });
+
+}());
