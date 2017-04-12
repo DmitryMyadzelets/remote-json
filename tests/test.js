82ed89cb9ba6bbd4c60e89090fbb7567449e8cd7
Dmitry Myadzelets <dmitrymyadzelets@gmail.comgit config --global user.email dmitrymyadzelets@gmail.com>
2017-04-12 14:24:51 +0200

    Updated for new versioni

--- tests/test.js
+++ tests/test.js
@@ -16,51 +16,45 @@ const assert = (function () {
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 
-console.log('Expected ' + 27 + ' tests passed');
+console.log('Expected ' + 25 + ' tests passed');
 
 
 const remote = require('../').remote;
 const json = remote(https, 'https://jsonplaceholder.typicode.com/posts');
 
 
-// http options
-assert(json.options.get.host === 'jsonplaceholder.typicode.com');
-assert(json.options.get.path === '/posts');
-
-
-// get
 json.get(function (err, res, body) {
     assert(!err);
     assert(200 === res.statusCode);
     assert(body instanceof Array);
-});
+})();
 
+json.post(function (err, res, body) {
+    assert(!err);
+    assert(201 === res.statusCode);
+    assert(true === body.ok);
+})({ok: true});
 
-// post, put, patch, del
-json
-    .post({ok: true}, function (err, res, body) {
-        assert(!err);
-        assert(201 === res.statusCode);
-        assert(true === body.ok);
-    })
-    .put({ok: true}, function (err, res, body) {
-        assert(!err);
-        assert(200 === res.statusCode);
-        assert(true === body.ok);
-        assert(1 === body.id);
-    }, '/1')
-    .patch({ok: true}, function (err, res, body) {
-        assert(!err);
-        assert(200 === res.statusCode);
-        assert(true === body.ok);
-        assert('string' === typeof body.title);
-        assert(2 === body.id);
-    }, '/2')
-    .del({}, function (err, res, body) {
-        assert(!err);
-        assert(200 === res.statusCode);
-        assert(!!body);
-    }, '/3');
+json.put('/1', function (err, res, body) {
+    assert(!err);
+    assert(200 === res.statusCode);
+    assert(true === body.ok);
+    assert(1 === body.id);
+})({ok: true});
+
+json.patch('/2', function (err, res, body) {
+    assert(!err);
+    assert(200 === res.statusCode);
+    assert(true === body.ok);
+    assert('string' === typeof body.title);
+    assert(2 === body.id);
+})({ok: true});
+
+json.del('/3', function (err, res, body) {
+    assert(!err);
+    assert(200 === res.statusCode);
+    assert(!!body);
+})();
 
 
 // Some additional tests
@@ -68,18 +62,18 @@ json
 
     remote(https, 'https://wrong.address').get(function (err) {
         assert(err instanceof Error);
-    });
+    })();
 
     remote(http, 'http://echo.jsontest.com/key/value/one/two').get(function (err, res, body) {
         assert(!err);
         assert(200 === res.statusCode);
         assert('object' === typeof body);
-    });
+    })();
 
     remote(http, 'http://validate.jsontest.com/?json=%7B%22key%22:%22value%22').get(function (err, res, body) {
         assert(!err);
         assert(200 === res.statusCode);
         assert('This error came from the org.json reference parser.' === body.error_info);
-    });
+    })();
 
 }());
