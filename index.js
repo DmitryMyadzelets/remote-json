4507263c6838fb52da7c4b48fdb3d8642ec28290
Dmitry Myadzelets <dmitrymyadzelets@gmail.comgit config --global user.email dmitrymyadzelets@gmail.com>
2017-10-18 23:50:06 +0200

    First try of es6

--- index.js
+++ index.js
@@ -1,154 +1,144 @@
-/*jslint browser: false*/
-'use strict';
+/* jslint browser: false */
+'use strict'
 
-const url = require('url');
-
+const url = require('url')
 
 // ============================================================================
 // Helpers
 
 // Returns a copy of the object
-function copy(object, dest) {
-    return Object.keys(object).reduce(function (o, key) {
-        o[key] = object[key];
-        return o;
-    }, dest || {});
+function copy (object, dest) {
+  return Object.keys(object).reduce((o, key) => {
+    o[key] = object[key]
+    return o
+  }, dest || {})
 }
-
 
 // Content types we parse as JSON
 const contentTypes = {
-    'application/json': true
-};
-
+  'application/json': true
+}
 
 // Checks if the given content type is one we accept
 // Note: NodeJS makes HTTP headers lower case by default
-function inContentTypes(type) {
-    type += '';
-    return Object.keys(contentTypes).some(function (ct) {
-        return type.includes(ct) && contentTypes[ct];
-    });
+function inContentTypes (type) {
+  type += ''
+  return Object.keys(contentTypes).some(ct => type.includes(ct) && contentTypes[ct])
 }
-
 
 // Returns HTTP response callback
 const response = (function () {
-    const CT = 'content-type';
+  const CT = 'content-type'
 
-    function data(d) {
-        if (undefined === this.body) {
-            this.body = '';
-        }
-        this.body += d;
+  function data (o, d) {
+    if (undefined === o.body) {
+      o.body = ''
+    }
+    o.body += d
+  }
+
+  function end (o, callback, res) {
+    // Parse accepted content types as JSON
+    if (!inContentTypes(res.headers[CT])) {
+      throw new Error(`Conten type ${res.headers[CT]}` +
+                      `failed the check against ${JSON.stringify(contentTypes)}`)
+    }
+    let data = {body: JSON.parse(o.body), res: res}
+    callback(data)
+  }
+
+  return function (callback, res) {
+    let o = {
+      body: undefined
     }
 
-    function end(callback, res) {
-        try {
-            // Parse accepted content types as JSON
-            if (!inContentTypes(res.headers[CT])) {
-                throw new Error('Content type \"'
-                        + res.headers[CT]
-                        + '\" failed the check against '
-                        + JSON.stringify(contentTypes));
-            }
-
-            callback(null, res, JSON.parse(this.body));
-
-        } catch (err) {
-            return callback(err, res, this.body);
-        }
-
-    }
-
-    return function (callback, res) {
-        const o = {
-            body: undefined
-        };
-
-        res.setEncoding('utf8')
-            .on('data', data.bind(o))
-            .on('end', end.bind(o, callback, res));
-    };
-}());
-
+    res.setEncoding('utf8')
+            .on('data', data.bind(undefined, o))
+            .on('end', end.bind(undefined, o, callback, res))
+  }
+}())
 
 // Returns a HTTP method
-function method(opt, path, data, callback) {
+function method (opt, path, data, callback) {
     // Check permutation of arguments
-    if ('function' === typeof path) {
-        callback = path;
-        path = undefined;
-        data = undefined;
-    } else if ('function' === typeof data) {
-        callback = data;
-        if ('string' === typeof path) {
-            data = undefined;
-        } else {
-            data = path;
-            path = undefined;
-        }
+  if (typeof path === 'function') {
+    callback = path
+    path = undefined
+    data = undefined
+  } else if (typeof data === 'function') {
+    callback = data
+    if (typeof path === 'string') {
+      data = undefined
+    } else {
+      data = path
+      path = undefined
     }
+  }
 
-    // Add path if any
-    if (path) {
-        opt = copy(opt);
-        opt.path += path;
-    }
+  // Add path if any
+  if (path) {
+    opt = copy(opt)
+    opt.path += path
+  }
 
-    const res = response.bind(this, callback);
-    const req = this.http.request(opt, res);
+  const res = response.bind(undefined, callback)
+  const req = this.http.request(opt, res)
 
-    req.on('error', callback);
+  req.on('error', e => { throw e })
 
-    if ('object' === typeof data && data !== null) {
-        req.write(JSON.stringify(data));
-    }
+  if (typeof data === 'object' && data !== null) {
+    req.write(JSON.stringify(data))
+  }
 
-    req.end();
+  req.end()
 }
 
 // ============================================================================
 //
 
-function Remote(uri, opt) {
-    const parsed = url.parse(uri);
-    opt = copy({
-        host: parsed.host,
-        path: parsed.path
-    }, opt);
-    opt.headers = opt.headers || {};
-    opt.headers['Content-Type'] = 'application/json';
+function Remote (uri, opt) {
+  const parsed = url.parse(uri)
+  opt = copy({
+    host: parsed.host,
+    path: parsed.path
+  }, opt)
+  opt.headers = opt.headers || {}
+  opt.headers['Content-Type'] = 'application/json'
 
-    switch (parsed.protocol) {
+  switch (parsed.protocol) {
     case 'https:':
-        if (!module.exports.https) {
-            module.exports.https = require('https');
-        }
-        this.http = module.exports.https;
-        break;
+      if (!module.exports.https) {
+        module.exports.https = require('https')
+      }
+      this.http = module.exports.https
+      break
     case 'http:':
-        if (!module.exports.http) {
-            module.exports.http = require('http');
-        }
-        this.http = module.exports.http;
-        break;
+      if (!module.exports.http) {
+        module.exports.http = require('http')
+      }
+      this.http = module.exports.http
+      break
     default:
-        throw new Error('Not supported protocol: ' + parsed.protocol);
-    }
+      throw new Error('Not supported protocol: ' + parsed.protocol)
+  }
 
-    // Constructors of HTTP methods
-    this.post = method.bind(this, copy(opt, {method: 'POST'}));
-    this.get = method.bind(this, copy(opt, {method: 'GET'}));
-    this.put = method.bind(this, copy(opt, {method: 'PUT'}));
-    this.del = method.bind(this, copy(opt, {method: 'DELETE'}));
-    this.patch = method.bind(this, copy(opt, {method: 'PATCH'}));
+  // Constructors of HTTP methods
+  // this.post = method.bind(this, copy(opt, {method: 'POST'}))
+  // this.get = method.bind(this, copy(opt, {method: 'GET'}))
+  // this.put = method.bind(this, copy(opt, {method: 'PUT'}))
+  // this.del = method.bind(this, copy(opt, {method: 'DELETE'}))
+  // this.patch = method.bind(this, copy(opt, {method: 'PATCH'}))
+  this.get = function (path, data) {
+    return new Promise((resolve) => {
+      return method.bind(this, copy(opt, {method: 'GET'}))(path, data, resolve)
+    })
+  }
 }
 
 // ============================================================================
 // Module exports
 module.exports = function (uri, opt) {
-    return new Remote(uri, opt);
-};
+  return new Remote(uri, opt)
+}
 
-module.exports.contentTypes = contentTypes;
+module.exports.contentTypes = contentTypes
--- tests/test.js
+++ tests/test.js
@@ -1,84 +1,92 @@
-/*jslint browser: false*/
-'use strict';
+/* jslint browser: false */
+'use strict'
 
 const assert = (function () {
-    var cnt = 0;
-    return function () {
-        require('assert').apply(this, arguments);
-        cnt += 1;
-        console.log('passed ' + cnt);
-    };
-}());
+  var cnt = 0
+  return function () {
+    require('assert').apply(this, arguments)
+    cnt += 1
+    console.log('passed ' + cnt)
+  }
+}())
 
 // Ignore HTTPS warnings
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 
+console.log('Expected ' + 26 + ' tests passed')
 
-console.log('Expected ' + 26 + ' tests passed');
+const remote = require('../')
+const json = remote('https://jsonplaceholder.typicode.com/posts')
 
+// Called when an unexpected error is catched
+const expectNoError = err => {
+  console.log(err)
+}
 
-const remote = require('../');
-const json = remote('https://jsonplaceholder.typicode.com/posts');
+json.get()
+  .then(({body, res}) => {
+    assert(res.statusCode === 200)
+    assert(body instanceof Array)
+  })
+  .catch(expectNoError)
 
+// json.get(function (err, res, body) {
+//     assert(!err);
+//     assert(200 === res.statusCode);
+//     assert(body instanceof Array);
+// });
 
-json.get(function (err, res, body) {
-    assert(!err);
-    assert(200 === res.statusCode);
-    assert(body instanceof Array);
-});
+// json.post({ok: true}, function (err, res, body) {
+//     assert(!err);
+//     assert(201 === res.statusCode);
+//     assert(true === body.ok);
+// });
 
-json.post({ok: true}, function (err, res, body) {
-    assert(!err);
-    assert(201 === res.statusCode);
-    assert(true === body.ok);
-});
+// json.put('/1', {ok: true}, function (err, res, body) {
+//     assert(!err);
+//     assert(200 === res.statusCode);
+//     assert(true === body.ok);
+//     assert(1 === body.id);
+// });
 
-json.put('/1', {ok: true}, function (err, res, body) {
-    assert(!err);
-    assert(200 === res.statusCode);
-    assert(true === body.ok);
-    assert(1 === body.id);
-});
+// json.patch('/2', {ok: true}, function (err, res, body) {
+//     assert(!err);
+//     assert(200 === res.statusCode);
+//     assert(true === body.ok);
+//     assert('string' === typeof body.title);
+//     assert(2 === body.id);
+// });
 
-json.patch('/2', {ok: true}, function (err, res, body) {
-    assert(!err);
-    assert(200 === res.statusCode);
-    assert(true === body.ok);
-    assert('string' === typeof body.title);
-    assert(2 === body.id);
-});
+// json.del('/3', function (err, res, body) {
+//     assert(!err);
+//     assert(200 === res.statusCode);
+//     assert(!!body);
+// });
 
-json.del('/3', function (err, res, body) {
-    assert(!err);
-    assert(200 === res.statusCode);
-    assert(!!body);
-});
+// // Some additional tests
+// (function () {
 
+//     remote('https://wrong.address').get(function (err) {
+//         assert(err instanceof Error);
+//     });
 
-// Some additional tests
-(function () {
+//     remote('http://echo.jsontest.com/key/value/one/two').get(function (err, res, body) {
+//         assert(!err);
+//         assert(200 === res.statusCode);
+//         assert('object' === typeof body);
+//     });
 
-    remote('https://wrong.address').get(function (err) {
-        assert(err instanceof Error);
-    });
+//     remote('http://validate.jsontest.com/?json=%7B%22key%22:%22value%22').get(function (err, res, body) {
+//         assert(!err);
+//         assert(200 === res.statusCode);
+//         assert('This error came from the org.json reference parser.' === body.error_info);
+//     });
 
-    remote('http://echo.jsontest.com/key/value/one/two').get(function (err, res, body) {
-        assert(!err);
-        assert(200 === res.statusCode);
-        assert('object' === typeof body);
-    });
+//     // Check the protocol should be valid
+//     try {
+//         remote('ftp://wrong.protocol.com');
+//     } catch (e) {
+//         assert(e.message === 'Not supported protocol: ftp:');
+//     }
 
-    remote('http://validate.jsontest.com/?json=%7B%22key%22:%22value%22').get(function (err, res, body) {
-        assert(!err);
-        assert(200 === res.statusCode);
-        assert('This error came from the org.json reference parser.' === body.error_info);
-    });
-
-    // Check the protocol should be valid
-    try {
-        remote('ftp://wrong.protocol.com');
-    } catch (e) {
-        assert(e.message === 'Not supported protocol: ftp:');
-    }
-
-}());
+// }());
